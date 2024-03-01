62f0fea609f47c0cde3ce957f6aba1c4d410f692
himakhaitan <himanshukhaitan108@gmail.com>
2024-03-02 01:14:15 +0530

    [Router Configured][Login/Signup init]

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/Global.css";
-import App from "./App";
+import router from "./router";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  }
-]);
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
