cd8eacc4e4e9ccba0266513f45b99086edf5269e
sudharshannellore <sudharshannellore@gmail.com>
2024-04-05 19:04:48 +0530

    404Page Added

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './styles/pages/Page404.css';
+const Page404 = ()=>{
+    return(<>
+    <div className="container">
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for might have been removed or is temporarily unavailable.</p>
+      <a href="">Go back to homepage</a>
+    </div>
+    
+    </>)
+}
+export default Page404;
